a388d6a2ee04f2f8f83010dc821bb2e98cdc12db
viswanathr73 <viswanathr73@gmail.com>
2025-05-10 12:45:08 +0530

    feature/backend-user controller completed

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getMyFriends, getRecommendedUsers,sendFriendRequest,acceptFriendRequest } from "../controllers/user.controller.js";
+import { getMyFriends, getRecommendedUsers,sendFriendRequest,acceptFriendRequest,getFriendRequests,getOutgoingFriendReqs } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
@@ -13,3 +13,5 @@ router.get("/friends", getMyFriends);
 
 router.post("/friend-request/:id", sendFriendRequest);
 router.put("/friend-request/:id/accept", acceptFriendRequest);
+router.get("/friend-requests", getFriendRequests);
+router.get("/outgoing-friend-requests", getOutgoingFriendReqs);
